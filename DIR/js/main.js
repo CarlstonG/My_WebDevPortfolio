b3bb2f4b3315536b80d110cb2b2c196eb635b88e
CarlstonG <carlstongrefalde@gmail.com>
2020-03-17 20:31:24 +0800

    JS menu btn addition

---
+++ DIR/js/main.js
@@ -0,0 +1,32 @@
+const menubtn = document.querySelector('.menu-btn');
+const menu= document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+//Close and open menu
+
+let showMenu = false;
+
+menubtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menubtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
+
+    } else {
+        menubtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
+    }
+}
